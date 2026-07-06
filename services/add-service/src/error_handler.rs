9598511173f2e9525f82b8bd8cb5a746033be4b7
XsamuraiX <matvej.otwagin@gmail.com>
2026-07-06 17:04:28 +0400

    Now it uses generated Operation function

--- services/add-service/src/error_handler.rs
+++ services/add-service/src/error_handler.rs
@@ -1,5 +1,6 @@
 use crate::init::ErrorResponse;
 use actix_web::{HttpRequest, error::QueryPayloadError};
+
 pub fn error_add(error: QueryPayloadError, _req: &HttpRequest) -> actix_web::Error {
     let clean_error = error
         .to_string()
@@ -11,4 +12,11 @@ pub fn error_add(error: QueryPayloadError, _req: &HttpRequest) -> actix_web::Err
         message: clean_error,
     };
     actix_web::error::ErrorBadRequest(response)
+}
+
+use std::fmt;
+impl fmt::Display for ErrorResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
 }
--- services/add-service/src/init.rs
+++ services/add-service/src/init.rs
@@ -1,23 +1,13 @@
-use serde::{Deserialize, Serialize};
-use std::fmt;
-#[derive(Deserialize)]
-pub struct Operation {
-    pub a: f64,
-    pub b: f64
-}
+use serde::{Serialize};
+
+pub use models::operation::Operation;
 
 #[derive(Serialize)]
 pub struct SuccessResponse {
-    pub result: f64
+    pub result: f64,
 }
 
 #[derive(Serialize, Debug)]
 pub struct ErrorResponse {
-    pub message: String
-}
-
-impl fmt::Display for ErrorResponse {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
+    pub message: String,
 }
