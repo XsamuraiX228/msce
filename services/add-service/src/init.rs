88b02b3525caaef7b592d89709214c96d7de0f2c
XsamuraiX <matvej.otwagin@gmail.com>
2026-07-06 17:49:31 +0400

    Save

--- services/add-service/src/init.rs
+++ services/add-service/src/init.rs
@@ -1,10 +1,5 @@
-use serde::{Deserialize, Serialize};
-use std::fmt;
-#[derive(Deserialize)]
-pub struct Operation {
-    pub a: f64,
-    pub b: f64
-}
+use serde::Serialize;
+pub use models::operation::Operation;
 
 #[derive(Serialize)]
 pub struct SuccessResponse {
@@ -14,10 +9,4 @@ pub struct SuccessResponse {
 #[derive(Serialize, Debug)]
 pub struct ErrorResponse {
     pub message: String
-}
-
-impl fmt::Display for ErrorResponse {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
 }
