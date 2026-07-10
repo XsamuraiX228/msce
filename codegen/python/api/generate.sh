a03baad9acc4d5d9b86d05528a9cce8cc59af1c7
q <weebucks2@gmail.com>
2026-07-10 18:39:33 +0300

    python codegen polishing

--- codegen/python/api/generate.sh
+++ codegen/python/api/generate.sh
@@ -3,10 +3,15 @@
 PACKAGE_PATH=packages/generated/python/api
 CODEGEN_PATH=codegen/python/api
 
+# Required for package
+if [ ! -d "src" ]; then
+    mkdir $PACKAGE_PATH/src
+endif
+
 # Generating models
 redocly bundle api/openapi.yaml | \
 datamodel-codegen \
-	--output $PACKAGE_PATH/__init__.py \
+	--output $PACKAGE_PATH/src/__init__.py \
 	--input-file-type openapi \
 	--formatters builtin
 
--- codegen/rust/api/generate.sh
+++ codegen/rust/api/generate.sh
@@ -5,18 +5,20 @@ CODEGEN_PATH=codegen/rust/api
 
 bundle=$(mktemp --suffix=.yaml)
 
+# Required for package
+if [ ! -d "src" ]; then
+    mkdir $PACKAGE_PATH/src
+endif
+
 # Generating models
 redocly bundle api/openapi.yaml > $bundle
 omg \
     --input $bundle \
-    --output $PACKAGE_PATH \
+    --output $PACKAGE_PATH/src \
     --mode models
 
-# Required for package
-mkdir $PACKAGE_PATH/src
-
 # Changing from module to library
-mv $PACKAGE_PATH/models.rs $PACKAGE_PATH/src/lib.rs
+mv $PACKAGE_PATH/src/models.rs $PACKAGE_PATH/src/lib.rs
 rm $PACKAGE_PATH/mod.rs
 
 # Creating Cargo.toml
